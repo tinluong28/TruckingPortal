b1ea3ab3fe391fb79948866a4ad51518973c76eb
tinluong28 <tinluong28@gmail.com>
2019-02-03 22:18:47 -0800

    prepare for deploy

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const profile = require("./routes/api/profile"),
   passport = require("passport"),
   bodyParser = require("body-parser"),
+  path = require("path"),
   users = require("./routes/api/users"),
   customer = require("./routes/api/customer"),
   delivery = require("./routes/api/delivery"),
@@ -34,6 +35,16 @@ app.use("/api/profile", profile);
 app.use("/api/customers", customer);
 app.use("/api/delivery", delivery);
 app.use("/api/file", deliveryOrder);
+
+// Server static assets if in production
+if (process.env.NODE_ENV === "production") {
+  // Set static folder
+  app.use(express.static("client/build"));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(_dirname, "client", "build", "index.html"));
+  });
+}
 
 // Console log server is up and running
 app.listen(port, process.env.IP, () => {
